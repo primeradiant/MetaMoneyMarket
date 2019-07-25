a12d553ee186fd9baf203251e5aa808ed4a0ed39
fvictorio <victorio.franco@gmail.com>
2019-07-25 11:07:44 -0300

    Add twitter and github links in footer

--- src/components/layout/ContentWrapper.tsx
+++ src/components/layout/ContentWrapper.tsx
@@ -1,3 +1,5 @@
+import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, {HTMLAttributes} from 'react';
 import styled from 'styled-components';
 
@@ -32,6 +34,10 @@ const ContentWrapper: React.FC<Props> = (props: Props) => {
     <ContentWrapperComponent {...restProps}>
       <ContentCenterStyled>{children}</ContentCenterStyled>
       <Footer>
+        <a href='https://twitter.com/metamoneymarket'><FontAwesomeIcon icon={faTwitter} /></a>
+        &nbsp;&nbsp;
+        <a href='https://github.com/Altoros/sovereign'><FontAwesomeIcon icon={faGithub} /></a>
+        &nbsp;&nbsp;
         Data provided by <a href="https://nomics.com/">Nomics</a>
       </Footer>
     </ContentWrapperComponent>
