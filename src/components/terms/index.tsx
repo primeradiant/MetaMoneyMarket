b8e49ff8288173643b4e95d800da298b7de75460
Jacob Shiach <kingjacob@gmail.com>
2019-08-13 15:31:20 -0700

    Update index.tsx

--- src/components/terms/index.tsx
+++ src/components/terms/index.tsx
@@ -8,7 +8,19 @@ const TermsContainer = styled.div``;
 const TermsAndConditions: React.FC<Props> = (props: Props) => {
   const {...restProps} = props;
 
-  return <TermsContainer {...restProps}>Tems and conditions</TermsContainer>;
+  return <TermsContainer {...restProps}>
+    <h2>Terms and conditions</h2>
+Please Do Not Deposit Funds except to test this smart contract. 
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+  
+  </TermsContainer>;
 };
 
 export default TermsAndConditions;
