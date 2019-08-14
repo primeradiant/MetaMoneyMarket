0907489e6672dce9941815ddfd4119af6bf0a125
Jacob Shiach <kingjacob@gmail.com>
2019-08-13 17:07:32 -0700

    Update Menu.tsx

--- src/components/header/Menu.tsx
+++ src/components/header/Menu.tsx
@@ -72,9 +72,9 @@ const Menu: React.FC<Props> = props => {
   return (
     <>
       <MenuContainer {...restProps}>
-        <MenuItemLink activeClassName="active">
+        <MenuItemLink activeClassName="active">	
           <a href="https://github.com/primeradiant/MetaMoneyMarket">Developers</a>
-        </MenuItemLink>
+         </MenuItemLink>
         {!context.account && <MenuItem onClick={openModal}>Login</MenuItem>}
         {context.account && <MenuItem onClick={logout}>Logout</MenuItem>}
       </MenuContainer>
