f5d8f466af2ece526c0d1d5380aa67acd21347e1
Jeff Reiner <theundeadghost@gmail.com>
2019-10-31 19:16:27 +0100

    :sparkles: Allow network id of 50 for the localhost

--- src/web3/connectors.ts
+++ src/web3/connectors.ts
@@ -7,13 +7,14 @@ const INFURA_API_KEY =
 const supportedNetworkURLs = {
   1: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
   4: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
-  42: `https://kovan.infura.io/v3/${INFURA_API_KEY}`,
   5: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
+  42: `https://kovan.infura.io/v3/${INFURA_API_KEY}`,
+  50: 'http://localhost:8545',
 };
 
 const defaultNetwork = 1;
 
-export const MetaMask = new Connectors.InjectedConnector({supportedNetworks: [1, 4, 42, 5]});
+export const MetaMask = new Connectors.InjectedConnector({supportedNetworks: [1, 4, 5, 42, 50]});
 
 export const Infura = new Connectors.NetworkOnlyConnector({
   providerURL: supportedNetworkURLs[1],
