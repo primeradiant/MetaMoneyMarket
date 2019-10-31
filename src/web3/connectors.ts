b4c73310fdadcbe97ba85414a9f174a37e1546c8
Jeff Reiner <theundeadghost@gmail.com>
2019-10-31 19:00:51 +0100

    :sparkles: Reworking Connectors to add WalletConnect and support networks better

--- src/web3/connectors.ts
+++ src/web3/connectors.ts
@@ -1,7 +1,27 @@
 import {Connectors} from 'web3-react';
+import WalletConnectSubprovider from '@walletconnect/web3-subprovider';
 
-export const MetaMask = new Connectors.InjectedConnector({supportedNetworks: [1, 4, 42, 50]});
+const INFURA_API_KEY =
+  process.env.NODE_ENV === 'production' ? process.env.REACT_APP_INFURA_API_KEY : 'dd1817d5225849d18bce57c75d4c23d1';
+
+const supportedNetworkURLs = {
+  1: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
+  4: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
+  42: `https://kovan.infura.io/v3/${INFURA_API_KEY}`,
+  5: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
+};
+
+const defaultNetwork = 1;
+
+export const MetaMask = new Connectors.InjectedConnector({supportedNetworks: [1, 4, 42, 5]});
 
 export const Infura = new Connectors.NetworkOnlyConnector({
-  providerURL: process.env.REACT_APP_RPC_URL || '',
+  providerURL: supportedNetworkURLs[1],
+});
+
+export const WalletConnect = new Connectors.WalletConnectConnector({
+  api: WalletConnectSubprovider,
+  bridge: 'https://bridge.walletconnect.org',
+  supportedNetworkURLs,
+  defaultNetwork,
 });
