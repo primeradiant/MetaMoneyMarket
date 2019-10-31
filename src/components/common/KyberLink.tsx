633149115f4b7cf965bfb3c0e6e8fec7b103a77e
Jeff Reiner <theundeadghost@gmail.com>
2019-10-31 21:07:25 +0100

    :bug: Fix the kyber button not init'n

--- src/components/common/KyberLink.tsx
+++ src/components/common/KyberLink.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Button } from 'rebass';
 import { KYBER_COMMISSION_ADDRESS } from '../../util/constants';
 
@@ -6,16 +6,25 @@ interface Props extends React.HTMLAttributes<any> {
   tokenSymbol: string;
 }
 
-const KyberLink: React.FC<Props> = ({ tokenSymbol, children, ...restProps }: Props) => (
-  <Button
-    as="a"
-    href={`https://widget.kyber.network/v0.7.2/?type=swap&mode=popup&lang=en&defaultPair=ETH_${tokenSymbol}&callback=https%3A%2F%2Fkyberpay-sample.knstats.com%2Fcallback&paramForwarding=true&network=mainnet&theme=theme-emerald&commissionId=${KYBER_COMMISSION_ADDRESS}`}
-    className="kyber-widget-button"
-    target="_blank"
-    {...restProps}
-  >
-    {children}
-  </Button>
-);
+const KyberLink: React.FC<Props> = ({ tokenSymbol, children, ...restProps }: Props) => {
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      // @ts-ignore
+      window.kyberWidgetOptions.register('#kyber-link');
+    }
+  }, []);
+
+  return (
+    <Button
+      as="a"
+      id="kyber-link"
+      href={`https://widget.kyber.network/v0.7.2/?type=swap&mode=popup&lang=en&defaultPair=ETH_${tokenSymbol}&callback=https%3A%2F%2Fkyberpay-sample.knstats.com%2Fcallback&paramForwarding=true&network=mainnet&theme=theme-emerald&commissionId=${KYBER_COMMISSION_ADDRESS}`}
+      target="_blank"
+      {...restProps}
+    >
+      {children}
+    </Button>
+  );
+};
 
 export default KyberLink;
